c655d70814482d99401c282058579f1958526292
Olivier FUXET <olivier.fuxet@gmail.com>
2016-04-24 18:44:41 +0200

    utilise un booleen au lieu d'une error dans la correction de l'ex 8 puisque les interfaces n'ont pas encore été vues

--- go100/08-exercices/README.md
+++ go100/08-exercices/README.md
@@ -2,7 +2,4 @@
 
 Ecrivez une fonction retournant l'inverse d'un nombre décimal donné en paramètre.
 
-Pensez au cas où le nombre est nul : il peut être intéressant de retourner une erreur dans ce cas particulier.
-Pour créer une erreur, vous pouvez utiliser :
-
-    errors.New("Le message d'erreur")
+Pensez au cas où le nombre est nul : il peut être intéressant de retourner un booléen indiquant si le calcul s'est bien déroulé ou non.
--- go100/08-exercices/correction/main.go
+++ go100/08-exercices/correction/main.go
@@ -1,30 +1,27 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
-//inverse prend un nombre décimal en paramètre et retourne son inverse. Si le nombre est nul, une erreur est retournée.
-func inverse(x float64) (float64, error) {
+//inverse prend un nombre décimal en paramètre et retourne son inverse. Si le nombre est nul, on retourne false (seconde valeur de retour)
+func inverse(x float64) (float64, bool) {
 	if x == 0 {
-		return 0, errors.New("L'inverse de zéro n'existe pas.")
+		return 0, false
 	}
-	return 1 / x, nil
+	return 1 / x, true
 }
 
 func main() {
 	// Calcul de l'inverse de 3
-	if inv, err := inverse(3); err == nil {
+	if inv, ok := inverse(3); ok {
 		fmt.Printf("L'inverse de %f est %f\n", 3.0, inv)
 	} else {
-		fmt.Println(err)
+		fmt.Printf("Impossible de calculer l'inverse de %f", 3.0)
 	}
 
 	// Calcul de l'inverse de 0 !
-	if inv, err := inverse(0); err == nil {
+	if inv, ok := inverse(0); ok {
 		fmt.Printf("L'inverse de %f est %f\n", 0.0, inv)
 	} else {
-		fmt.Println(err)
+		fmt.Printf("Impossible de calculer l'inverse de %f\n", 0.0)
 	}
 }
